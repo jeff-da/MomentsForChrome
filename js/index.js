598351eaf4d7492953c4a18d89c7a900c9ee89d0
jeff-da <jzda@uw.edu>
2016-12-25 22:51:26 -0800

    First update on 12.25

--- js/index.js
+++ js/index.js
@@ -1,27 +1,25 @@
 $(document).ready(function(){
 	var d = new Date();
 	var n = d.getDate();
-	if (n % 11 == 0) {
-		document.body.className = "eleven";
-	} else if (n % 11 == 1) {
-		document.body.className = "one";
-	} else if (n % 11 == 2) {
-		document.body.className = "two";
-	} else if (n % 11 == 3) {
-		document.body.className = "three";
-	} else if (n % 11 == 4) {
-		document.body.className = "four";
-	} else if (n % 11 == 5) {
-		document.body.className = "five";
-	} else if (n % 11 == 6) {
-		document.body.className = "six";
-	} else if (n % 11 == 7) {
-		document.body.className = "seven";
-	} else if (n % 11 == 8) {
-		document.body.className = "eight";
-	} else if (n % 11 == 9) {
-		document.body.className = "nine";
-	} else {
+	if (n % 10 == 0) {
 		document.body.className = "ten";
+	} else if (n % 10 == 1) {
+		document.body.className = "one";
+	} else if (n % 10 == 2) {
+		document.body.className = "two";
+	} else if (n % 10 == 3) {
+		document.body.className = "three";
+	} else if (n % 10 == 4) {
+		document.body.className = "four";
+	} else if (n % 10 == 5) {
+		document.body.className = "five";
+	} else if (n % 10 == 6) {
+		document.bod0.className = "six";
+	} else if (n % 10== 7) {
+		document.body.className = "seven";
+	} else if (n % 10 == 8) {
+		document.body.className = "eight";
+	} else if (n % 10 == 9) {
+		document.body.className = "nine";
 	}
 });
