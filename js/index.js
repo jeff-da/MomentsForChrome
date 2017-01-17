d833b13f5b6000fe074cefd1b2b9ae1ab3edc0ef
jeff-da <jzda@uw.edu>
2017-01-17 11:57:19 -0800

    Updated with new images 1.17.2017

--- js/index.js
+++ js/index.js
@@ -1,25 +1,30 @@
 $(document).ready(function(){
 	var d = new Date();
 	var n = d.getDate();
-	if (n % 10 == 0) {
-		document.body.className = "ten";
-	} else if (n % 10 == 1) {
+	n = n + 2;
+	if (n % 12 == 0) {
+		document.body.className = "twelve";
+	} else if (n % 12 == 1) {
 		document.body.className = "one";
-	} else if (n % 10 == 2) {
+	} else if (n % 12 == 2) {
 		document.body.className = "two";
-	} else if (n % 10 == 3) {
+	} else if (n % 12 == 3) {
 		document.body.className = "three";
-	} else if (n % 10 == 4) {
+	} else if (n % 12 == 4) {
 		document.body.className = "four";
-	} else if (n % 10 == 5) {
+	} else if (n % 12 == 5) {
 		document.body.className = "five";
-	} else if (n % 10 == 6) {
+	} else if (n % 12 == 6) {
 		document.body.className = "six";
-	} else if (n % 10 == 7) {
+	} else if (n % 12 == 7) {
 		document.body.className = "seven";
-	} else if (n % 10 == 8) {
+	} else if (n % 12 == 8) {
 		document.body.className = "eight";
-	} else {
+	} else if (n % 12 == 9) {
 		document.body.className = "nine";
+	} else if (n % 12 == 10) {
+				document.body.className = "ten";
+	} else if (n % 12 == 11) {
+				document.body.className = "eleven";
 	}
 });
