6fe8cda841338544d75095dfe3a17d2e07a3dea4
Jeffrey Da <jexelD@gmail.com>
2017-08-26 13:30:10 -0700

    Update code to use React

--- js/index.js
+++ js/index.js
@@ -2,37 +2,43 @@ $(document).ready(function(){
 	var d = new Date();
 	var n = d.getDate();
 	n = n + 2;
-	if (n % 16 == 0) {
-		document.body.className = "sixteen";
-	} else if (n % 16 == 1) {
+	if (n % 19 == 0) {
+		document.body.className = "nineteen";
+	} else if (n % 19 == 1) {
 		document.body.className = "one";
-	} else if (n % 16 == 2) {
+	} else if (n % 19 == 2) {
 		document.body.className = "two";
-	} else if (n % 16 == 3) {
+	} else if (n % 19 == 3) {
 		document.body.className = "three";
-	} else if (n % 16 == 4) {
+	} else if (n % 19 == 4) {
 		document.body.className = "four";
-	} else if (n % 16 == 5) {
+	} else if (n % 19 == 5) {
 		document.body.className = "five";
-	} else if (n % 16 == 6) {
+	} else if (n % 19 == 6) {
 		document.body.className = "six";
-	} else if (n % 16 == 7) {
+	} else if (n % 19 == 7) {
 		document.body.className = "seven";
-	} else if (n % 16 == 8) {
+	} else if (n % 19 == 8) {
 		document.body.className = "eight";
-	} else if (n % 16 == 9) {
+	} else if (n % 19 == 9) {
 		document.body.className = "nine";
-	} else if (n % 16 == 10) {
+	} else if (n % 19 == 10) {
 				document.body.className = "ten";
-	} else if (n % 16 == 11) {
+	} else if (n % 19 == 11) {
 				document.body.className = "eleven";
-	} else if (n % 16 == 12) {
+	} else if (n % 19 == 12) {
 				document.body.className = "twelve";
-	} else if (n % 16 == 13) {
+	} else if (n % 19 == 13) {
 				document.body.className = "thirteen";
-	} else if (n % 16 == 14) {
+	} else if (n % 19 == 14) {
 				document.body.className = "fourteen";
-	} else if (n % 16 == 15) {
+	} else if (n % 19 == 15) {
 				document.body.className = "fifteen";
+	} else if (n % 19 == 16) {
+				document.body.className = "sixteen";
+	} else if (n % 19 == 17) {
+				document.body.className = "seventeen";
+	} else if (n % 19 == 18) {
+				document.body.className = "eighteen";
 	}
 });
